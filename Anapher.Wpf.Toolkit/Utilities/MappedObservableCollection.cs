5906406f4ecd45fbeacf22b43e82016a29755fab
Vincent (Anapher) <anapher@protonmail.com>
2019-01-13 16:24:19 +0100

    MappedObservableCollection support Reset

--- Anapher.Wpf.Toolkit/Utilities/MappedObservableCollection.cs
+++ Anapher.Wpf.Toolkit/Utilities/MappedObservableCollection.cs
@@ -13,6 +13,7 @@ namespace Anapher.Wpf.Toolkit.Utilities
     /// <typeparam name="TModel">The model (source object)</typeparam>
     public class MappedObservableCollection<TViewModel, TModel> : ObservableCollection<TViewModel>
     {
+        private readonly ObservableCollection<TModel> _sourceCollection;
         private readonly Func<TModel, TViewModel> _viewModelFactory;
         private readonly Dictionary<TModel, TViewModel> _viewModels = new Dictionary<TModel, TViewModel>();
 
@@ -23,6 +24,7 @@ namespace Anapher.Wpf.Toolkit.Utilities
         /// <param name="viewModelFactory">The factory that creates a <see cref="TViewModel"/> from a <see cref="TModel"/></param>
         public MappedObservableCollection(ObservableCollection<TModel> sourceCollection, Func<TModel, TViewModel> viewModelFactory)
         {
+            _sourceCollection = sourceCollection;
             _viewModelFactory = viewModelFactory;
 
             foreach (var model in sourceCollection)
@@ -43,9 +45,10 @@ namespace Anapher.Wpf.Toolkit.Utilities
             switch (e.Action)
             {
                 case NotifyCollectionChangedAction.Add:
-                    foreach (var model in e.NewItems.Cast<TModel>()) Add(CreateViewModel(model));
-                    break;
                 case NotifyCollectionChangedAction.Remove:
+                case NotifyCollectionChangedAction.Replace:
+                    foreach (var model in e.NewItems.Cast<TModel>()) Add(CreateViewModel(model));
+
                     foreach (var model in e.OldItems.Cast<TModel>())
                     {
                         var viewModel = _viewModels[model];
@@ -53,6 +56,12 @@ namespace Anapher.Wpf.Toolkit.Utilities
 
                         Remove(viewModel);
                     }
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    Clear();
+
+                    foreach (var model in _sourceCollection)
+                        Add(CreateViewModel(model));
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
